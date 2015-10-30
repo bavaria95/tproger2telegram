2ea2c334c202d2fface91f4cb15cfa3b48a588c9
Dmitry Petruk <bavaria95@gmail.com>
2015-10-30 16:36:05 +0100

    sending posts via tg bot

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 # require 'sinatra'
 require 'vk-ruby'
 require 'redis'
+require 'telegram/bot'
 
 
 def get_posts
@@ -12,7 +13,22 @@ end
 host = "pub-redis-13908.us-east-1-4.5.ec2.garantiadata.com"
 port = 13908
 redis_pass = File.read('redis_pass.dat')
-
 redis = Redis.new(:host => host, :port => port, :password => redis_pass)
 
+token = File.read('token.dat')
+
 posts = get_posts
+post = posts[3]
+
+Telegram::Bot::Client.run(token) do |bot|
+	bot.api.sendMessage(chat_id: '@VK_tproger', text: post["text"])
+	post["attachments"].each do |attachment|
+		if attachment["type"] == "link"
+			bot.api.sendMessage(chat_id: '@VK_tproger', text: attachment["link"]["url"])
+		elsif attachment["type"] == "photo"
+			bot.api.sendMessage(chat_id: '@VK_tproger', text: "there should be some photo")
+		elsif attachment["type"] == "video"
+			bot.api.sendMessage(chat_id: '@VK_tproger', text: "there should be some video URL")
+		end
+	end
+end
