872738d309dc59cebb99939e35158ecd5e2bdd88
Dmitry Petruk <bavaria95@gmail.com>
2015-10-30 18:53:38 +0100

    processing attachments

--- app.rb
+++ app.rb
@@ -10,6 +10,11 @@ def get_posts
 	posts["items"]
 end
 
+def return_largest_photo attr_photos
+	photo = attr_photos.keep_if {|x| /photo_\d+/ =~ x}.sort_by{|k, v| k.split(/\D/).join.to_i}
+	photo[-1][-1] # returns value of last element in sorted structure
+end
+
 host = "pub-redis-13908.us-east-1-4.5.ec2.garantiadata.com"
 port = 13908
 redis_pass = File.read('redis_pass.dat')
@@ -18,17 +23,27 @@ redis = Redis.new(:host => host, :port => port, :password => redis_pass)
 token = File.read('token.dat')
 
 posts = get_posts
-post = posts[3]
 
 Telegram::Bot::Client.run(token) do |bot|
-	bot.api.sendMessage(chat_id: '@VK_tproger', text: post["text"])
-	post["attachments"].each do |attachment|
-		if attachment["type"] == "link"
-			bot.api.sendMessage(chat_id: '@VK_tproger', text: attachment["link"]["url"])
-		elsif attachment["type"] == "photo"
-			bot.api.sendMessage(chat_id: '@VK_tproger', text: "there should be some photo")
-		elsif attachment["type"] == "video"
-			bot.api.sendMessage(chat_id: '@VK_tproger', text: "there should be some video URL")
+	posts.each do |post|
+		text = post["text"]
+
+		if post.has_key? 'attachments'
+			post["attachments"].each do |attachment|
+				if attachment["type"] == "link"
+					a = attachment["link"]
+					text << "\n" + a["url"]
+				elsif attachment["type"] == "photo"
+					text << "\n" + return_largest_photo(attachment['photo'])
+				elsif attachment["type"] == "video"
+					# puts "video"
+					# puts attachment
+				end
+			end
+		end
+
+		if text.length >= 16
+			bot.api.sendMessage(chat_id: '@VK_tproger', text: text)
 		end
 	end
 end
